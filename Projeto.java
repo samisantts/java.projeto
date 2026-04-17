ab45068d216bf97795259bacb4543025215c463b
sami Gonçalves santos <samysantos993@gmail.com>
2026-04-17 17:34:24 -0300

    Add files via upload
    Hoje pratiquei switch/case em Java e finalmente entendi como funcionam case, break e default.
     No começo apanhei bastante com sintaxe e lógica, mas consegui resolver sozinho e isso fez toda diferença.
     Cada erro está me ajudando a aprender mais.

---
+++ Projeto.java
@@ -0,0 +1,55 @@
+package app;
+import java.util.Scanner;
+
+public class Projeto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);{
+
+       String opcao;
+
+       System.out.println("Escolha 1 numero");
+        System.out.print(" 1: cadastrar ");
+        System.out.print("2: login ");
+        System.out.print("3: ver perfil ");
+        System.out.println("4: sair ");
+        opcao = scanner.nextLine();
+
+
+
+        switch (opcao) {
+            case "1":
+                System.out.print("cadastrar");
+                break;
+            case "2":
+                System.out.print("login");
+                break;
+            case "3":
+                System.out.print("ver perfil");
+            break;
+            case "sair":
+                System.out.print("sair");
+                break;
+            default: System.out.print ("opcao invalida");
+
+
+
+
+
+
+        }
+
+
+
+        }
+
+
+
+            }
+
+        }
+
+
+
+
+
+
