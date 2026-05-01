565ed7844e10ebb86bf56946c0de7bc90f074ac3
sami Gonçalves santos <samysantos993@gmail.com>
2026-05-01 14:26:31 -0300

    Change opcao type from String to int in Projeto.java

--- Projeto.java
+++ Projeto.java
@@ -5,7 +5,7 @@ public class Projeto {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);{
 
-       String opcao;
+      int opcao;
 
        System.out.println("Escolha 1 numero");
         System.out.print(" 1: cadastrar ");
